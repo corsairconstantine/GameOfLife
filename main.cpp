76236798326afeb5f046c583cf32ea2a4f28ba16
corsairconstantine <corsair.constantine@yahoo.com>
2022-10-26 15:54:11 +0800

    added render function

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ vector <vector <int>> deadState(int w, int h) {
             row.push_back(0);
         st.push_back(row);
     }
-    cout << "made a dead state " << w << 'x' << h << endl;
+    cout << "made a dead state " << st[0].size() << 'x' << st.size() << endl;
     return st;
 }
 vector <vector <int>> randomState(int w, int h) {
@@ -21,25 +21,28 @@ vector <vector <int>> randomState(int w, int h) {
             row.push_back(rand() % 2);
         st.push_back(row);
     }
-    cout << "made a random state " << w << 'x' << h << endl;
+    cout << "made a random state " << st[0].size() << 'x' << st.size() << endl;
     return st;
 
 }
 
-int main() {
-    vector <vector <int>> st = deadState(5,5);
-
+void render(vector <vector <int>> st) {
     for (int height = 0; height < st.size(); ++height) {
         for (int width = 0; width < st[0].size(); ++width)
-            cout << st[width][height] << ' ';
+            if (st[height][width] == 1)
+                cout << 'o' << ' ';
+            else
+                cout << '.' << ' ';
         cout << endl;
     }
+}
 
-    vector <vector <int>> stRand = randomState(5,5);
-    for (int height = 0; height < stRand.size(); ++height) {
-        for (int width = 0; width < stRand[0].size(); ++width)
-            cout << stRand[width][height] << ' ';
-        cout << endl;
-    }
+int main() {
+    vector <vector <int>> st = deadState(10,20);
+    render(st);
+
+    vector <vector <int>> stRand = randomState(20,10);
+    render(stRand);
+
     return 0;
 }
